81d7de1935179c2da681096a4ca5ca742333aa77
luisrodriguez <betowas12345@hotmail.com>
2016-07-28 00:57:49 -0500

    pizzas

--- Appizzas/ViewController.swift
+++ Appizzas/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var tamaño2 = ""
+    
+    @IBOutlet weak var chica: UIButton!
+    @IBOutlet weak var mediana: UIButton!
+    @IBOutlet weak var grande: UIButton!
+    @IBOutlet weak var siguiente: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let envio=imprime()
+        let sigVista=segue.destinationViewController as! ViewMasa
+        sigVista.textam=envio
+    }
+    
+    
+    @IBAction func bottam(sender: AnyObject) {
+        imprime()
+    }
+    
+    func imprime()->String{
+        var textam = String(tamaño2)
+        
+        if textam == "" {
+            textam = "NO ELEGISTE TAMAÑO"
+        }
+        
+        return textam
+    }
+    
+    @IBAction func chicaSelected(sender: AnyObject) {
+        self.tamaño2 =  "CHICA"
+        chica.backgroundColor = UIColor.blackColor()
+        mediana.backgroundColor = UIColor.clearColor()
+        grande.backgroundColor = UIColor.clearColor()
+    }
+    
+    
+    @IBAction func medianaSelected(sender: AnyObject) {
+        self.tamaño2 = "MEDIANA"
+        mediana.backgroundColor = UIColor.blackColor()
+        chica.backgroundColor = UIColor.clearColor()
+        grande.backgroundColor = UIColor.clearColor()
+    }
+    
+    
+    
+    @IBAction func grandeSelected(sender: AnyObject) {
+        self.tamaño2 = "GRANDE"
+        grande.backgroundColor = UIColor.blackColor()
+        chica.backgroundColor = UIColor.clearColor()
+        mediana.backgroundColor = UIColor.clearColor()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Appizzas/ViewMasa.swift
@@ -0,0 +1,86 @@
+//
+//  ViewMasa.swift
+//  Appizzas
+//
+//  Created by Luis Rodriguez on 28/07/16.
+//  Copyright © 2016 Luis Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewMasa: UIViewController {
+    
+    var masa = ""
+    
+    @IBOutlet weak var delgada: UIButton!
+    @IBOutlet weak var gruesa: UIButton!
+    @IBOutlet weak var crujiente: UIButton!
+    @IBOutlet weak var tamañores: UILabel!
+    
+    var textam = ""
+    
+    override func viewWillAppear(animated: Bool) {
+        tamañores.text=String(textam)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func botmas(sender: AnyObject) {
+        imprime2()
+    }
+    
+    func imprime2()->String{
+        var texmas = String(masa)
+        
+        if texmas == "" {
+            texmas = "NO ELEGISTE MASA"
+        }
+        
+        return texmas
+    }
+    
+    
+    @IBAction func delgadaSelected(sender: AnyObject) {
+        self.masa = "DELGADA"
+        delgada.backgroundColor = UIColor.blackColor()
+        gruesa.backgroundColor = UIColor.clearColor()
+        crujiente.backgroundColor = UIColor.clearColor()
+    }
+    
+    
+    @IBAction func gruesaSelected(sender: AnyObject) {
+        self.masa = "GRUESA"
+        gruesa.backgroundColor = UIColor.blackColor()
+        delgada.backgroundColor = UIColor.clearColor()
+        crujiente.backgroundColor = UIColor.clearColor()
+    }
+    
+    
+    @IBAction func crujienteSelected(sender: AnyObject) {
+        self.masa = "CRUJIENTE"
+        crujiente.backgroundColor = UIColor.blackColor()
+        gruesa.backgroundColor = UIColor.clearColor()
+        delgada.backgroundColor = UIColor.clearColor()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
